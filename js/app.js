6d6c56f2712651827096ff926d57667001e5fe8c
Adam Edward <adavismarketer@gmail.com>
2021-03-16 09:54:53 -0400

    init

---
+++ js/app.js
@@ -0,0 +1,50 @@
+const fitbitAccessToken =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkM1VEQiLCJzdWIiOiI4TU5MUUQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJsb2MgcmhyIHJwcm8gcnNsZSIsImV4cCI6MTYxODM2OTYyOSwiaWF0IjoxNjE1Nzc3NjI5fQ.IbQCdHRiBUCcp2KIcqQKfGbX0XqGDfQdyqG_PTdnB5o";
+let seconds = 0;
+
+function getHeartrate() {
+  fetch(
+    "https://api.fitbit.com/1/user/-/activities/heart/date/today/1d/1sec.json",
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + fitbitAccessToken },
+    }
+  )
+    .then((response) => response.json())
+    .then((timeSeries) => {
+      const seriesLength =
+        timeSeries["activities-heart-intraday"].dataset.length - 1;
+      console.log(
+        timeSeries["activities-heart-intraday"].dataset[seriesLength].value
+      );
+    });
+  setTimeout(getHeartrate, 30000);
+}
+
+function counter(seconds) {
+  while (seconds < 30) {
+    setInterval(() => {
+      console.log(seconds);
+      seconds++;
+    });
+  }
+}
+
+
+// getHeartrate();
+
+//Proper Function of the working above
+// async function getFitbitData() {
+//   const accessToken =
+//     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkM1VEQiLCJzdWIiOiI4TU5MUUQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJsb2MgcmhyIHJwcm8gcnNsZSIsImV4cCI6MTYxODM2OTYyOSwiaWF0IjoxNjE1Nzc3NjI5fQ.IbQCdHRiBUCcp2KIcqQKfGbX0XqGDfQdyqG_PTdnB5o";
+//   const heartRateUrl =
+//     "https://api.fitbit.com/1/user/-/activities/heart/date/today/1d/1sec.json";
+
+//   const response = await fetch(heartRateUrl, {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: "Bearer " + accessToken,
+//     },
+//   }).then(processResponse);
+// }
---
+++ js/snips.js
@@ -0,0 +1,20 @@
+//New User Authentication redirect
+let fitbitAccessToken;
+
+if (!window.location.hash) {
+  window.location.replace(
+    "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=227H22&redirect_uri=https%3A%2F%2F15359f83.ngrok.io%2F&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight"
+  );
+} else {
+  let fragmentQueryParameters = {};
+  window.location.hash
+    .slice(1)
+    .replace(
+      new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+      function ($0, $1, $2, $3) {
+        fragmentQueryParameters[$1] = $3;
+      }
+    );
+
+  fitbitAccessToken = fragmentQueryParameters.access_token;
+}
