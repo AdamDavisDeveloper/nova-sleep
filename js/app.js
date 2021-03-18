7dd9435295408ac6ec41c1debdfd58f45a0cbb90
Adam Edward <adavismarketer@gmail.com>
2021-03-18 07:37:17 -0400

    compare function for rest rate and current rate

--- js/app.js
+++ js/app.js
@@ -2,11 +2,6 @@
 const fitbitAccessToken =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkM1VEQiLCJzdWIiOiI4TU5MUUQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJsb2MgcmhyIHJwcm8gcnNsZSIsImV4cCI6MTYxODM2OTYyOSwiaWF0IjoxNjE1Nzc3NjI5fQ.IbQCdHRiBUCcp2KIcqQKfGbX0XqGDfQdyqG_PTdnB5o";
 
-//Store daily resting heart rate
-function getRest(restData) {
-  console.log(restData);
-}
-
 //Get most recent heart rate reported of user
 //Updated every 30sec (per the API constraints)
 function getHeartrate() {
@@ -18,26 +13,24 @@ function getHeartrate() {
     }
   )
     .then((response) => response.json())
-    .then((timeSeries) => {
-      const seriesLength =
-        timeSeries["activities-heart-intraday"].dataset.length - 1;
-      console.log(
-        timeSeries["activities-heart-intraday"].dataset[seriesLength].value
-      );
-    });
+    .then(compare);
   setTimeout(getHeartrate, 30000);
 }
 
-//Fetch daily resting heart rate
-function restingHeartrate() {
-  fetch("https://api.fitbit.com/1/user/-/activities/heart/date/today/1d.json", {
-    method: "GET",
-    headers: { Authorization: "Bearer " + fitbitAccessToken },
-  })
-    .then((response) => response.json())
-    .then((json) =>
-      getRest(json["activities-heart"][0].value.restingHeartRate)
-    );
+function compare(json) {
+  const seriesLength = json["activities-heart-intraday"].dataset.length - 1;
+  let currentHr = json["activities-heart-intraday"].dataset[seriesLength].value;
+  let restHr = json["activities-heart"][0].value.restingHeartRate;
+  console.log("Current: " + currentHr);
+  console.log("Daily Resting: " + restHr);
+
+  if (currentHr < restHr) {
+    console.log("asleep");
+  } else if (currentHr > restHr) {
+    console.log("awake!");
+  } else {
+    console.log("Error");
+  }
 }
 
-restingHeartrate();
+// getHeartrate();
