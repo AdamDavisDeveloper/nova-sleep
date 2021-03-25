704884ffd25a32203cb2a826067ea024881350e7
Adam Davis <adavisdeveloper@gmail.com>
2021-03-25 02:28:40 -0400

    add stop functionality to getHeartrate()

--- js/app.js
+++ js/app.js
@@ -62,11 +62,12 @@ function change(userState) {
 function play(bool) {
   let isPlaying = bool;
   let playTime = time();
+
+  //start sleep timer fetch
+  getHeartrate();
   //UI display app start time
   timeStart.innerHTML = playTime;
   if (!isPlaying) {
     timeStop.innerHTML = playTime;
   }
 }
-//
-// getHeartrate();
